79aae49c5caca8504c2580b5ede1e3b1c53f9e12
Oliver Forral <intrepion@gmail.com>
2022-09-15 18:27:11 -0600

    Got Hello World working.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    HttpResponse::Ok().content_type("application/json").body("{\"id\":\"00000000-0000-0000-0000-000000000000\",\"jsonrpc\":\"2.0\",\"result\":{\"greeting\":\"Hello, World!\"}}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let client_url = env::var("CLIENT_URL").expect("You must set CLIENT_URL");
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .wrap(
+                Cors::default()
+                    .allowed_origin(&client_url)
+                    .allowed_methods(vec!["POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600),
+            )
+            .route("/", web::post().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
